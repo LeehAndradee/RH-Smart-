a7f49138624a0ba1e263804fa074ca433a2e5c01
Leticia Andrade <leticiaandradeoliveira@outlook.com>
2026-05-10 20:23:18 -0300

    Create super usuario

--- rhsmart/wsgi.py
+++ rhsmart/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rhsmart.settings')
 
 application = get_wsgi_application()
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@email.com', '123456789')
+    print("Superusuário 'admin' criado com sucesso!")
