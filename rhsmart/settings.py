08f19766f4e4528c1eead870538f34661bbd9f05
Leticia Andrade <leticiaandradeoliveira@outlook.com>
2026-05-10 19:47:47 -0300

    database/3

--- rhsmart/settings.py
+++ rhsmart/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-df8w0zb!%3olt+6bl0l7%k^gkf
 # No Railway, coloque a variável DEBUG como False.
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-# '*' permite acesso de qualquer domínio gerado pelo Railway
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # --- DEFINIÇÃO DE APLICATIVOS ---
 INSTALLED_APPS = [
