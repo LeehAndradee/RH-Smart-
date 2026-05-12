1a7c84a088c13019a1fc29f87e3cce7454c3980d
Leticia Andrade <leticiaandradeoliveira@outlook.com>
2026-05-12 19:56:42 -0300

    ....

--- rhsmart/wsgi.py
+++ rhsmart/wsgi.py
@@ -6,11 +6,19 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Define as configurações do projeto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rhsmart.settings')
 
-# Inicia a aplicação WSGI
 application = get_wsgi_application()
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@email.com', '123456789')
+    print("Superusuário 'admin' criado com sucesso!")
